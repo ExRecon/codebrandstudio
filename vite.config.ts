d6c40d7dfd6fc4fb6eef7f42bec60606c8072821
aliihtsham-debug <exreconnaissance@gmail.com>
2026-05-19 15:23:25 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/codebrandstudio/',
+  base: process.env.VERCEL ? '/' : '/codebrandstudio/',
   plugins: [react()],
   build: {
     rollupOptions: {
